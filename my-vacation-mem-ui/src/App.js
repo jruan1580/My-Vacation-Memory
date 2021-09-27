297944bb1a69f19f54e0f99d9d5eccbdd133daac
Jason Ruan <jason.ruan@lytx.com>
2021-09-27 10:57:17 -0700

    quick work for trip detail ui component

--- my-vacation-mem-ui/src/App.js
+++ my-vacation-mem-ui/src/App.js
@@ -1,13 +1,15 @@
 import NavigationBar from "./NavigationBar";
 import Container from "react-bootstrap/Container";
 import Home from "./Home";
+import TripDetails from "./TripDetails";
 
 function App() {
   return (
     <>
       <NavigationBar />
       <Container>
-        <Home />                  
+        {/* <Home />                   */}
+        <TripDetails />
       </Container>
     </>    
   );
---
+++ my-vacation-mem-ui/src/TripDetails/index.js
@@ -0,0 +1,19 @@
+function TripDetails(){
+    return(
+        <>
+            <div className="mt-4">
+                <h3>Trip to Japan</h3>
+            </div>
+            <div className="mt-1">
+                <span><b>Start:</b> 2019-07-03</span><br/>
+                <span><b>End:</b> 2019-07-03</span><br/>
+                <span><b>Rating:</b> 4/5</span>
+            </div>
+            <div className="mt-1">
+                <p><b>Description:</b> Went to Florence, Pisa, Venice, and Rome.</p>
+            </div>
+        </>
+    )
+}
+
+export default TripDetails;
